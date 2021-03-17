d59b7a12b06ded96211cd8637cf3dcfde3360deb
Misha Lysak <myhaillysak@gmail.com>
2021-03-17 23:33:21 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,12 +3,38 @@
 const listUrl
   = 'https://mate-academy.github.io/phone-catalogue-static/api/phones.json';
 
+const getAllSuccessfulDetails = () => {
+  return new Promise((resolve, reject) => {
+    fetch(listUrl)
+      .then(response => response.json())
+      .then(response => Promise.all(response))
+      .then(response => resolve(response.map(item => item.id)));
+  });
+};
+
+const listOfPhones = (dataFromServer) => {
+  const div = document.createElement('div');
+
+  div.classList.add('all-successful');
+
+  div.insertAdjacentHTML('afterbegin', `
+    <h3>All Successful</h3>
+    <ul>
+      ${dataFromServer.map(phone => (
+    `<li>${phone.toUpperCase()}</li>`)
+  ).join(' ')}
+    </ul>
+  `);
+
+  document.body.append(div);
+};
+
 const getFirstReceivedDetails = () => {
   return new Promise((resolve, reject) => {
     fetch(listUrl)
       .then(response => response.json())
-      .then(response => Promise.any(response))
-      .then(response => resolve(response));
+      .then(response => Promise.race(response))
+      .then(response => resolve(response.id));
   });
 };
 
@@ -20,31 +46,7 @@ const showFirstPhone = (url) => {
   div.insertAdjacentHTML('afterbegin', `
     <h3>First Received</h3>
     <ul>
-      <li>${url.id}</li>
-    </ul>
-  `);
-
-  document.body.append(div);
-};
-
-const getAllSuccessfulDetails = () => {
-  return new Promise((resolve, reject) => {
-    fetch(listUrl)
-      .then(response => response.json())
-      .then(response => Promise.all(response))
-      .then(response => resolve(response));
-  });
-};
-
-const listOfPhones = (dataFromServer) => {
-  const div = document.createElement('div');
-
-  div.classList.add('all-successful');
-
-  div.insertAdjacentHTML('afterbegin', `
-    <h3>All Sucessful</h3>
-    <ul>
-      ${dataFromServer.map(phone => `<li>${phone.id}</li>`).join(' ')}
+      <li>${url}</li>
     </ul>
   `);
 
